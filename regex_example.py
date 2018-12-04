ab04791d9fe4d990e318863e41c7f22aaa634772
Szymon Nowak <sznowak13@gmail.com>
2018-12-04 20:06:47 +0100

    Added more examples

--- regex_example.py
+++ regex_example.py
@@ -1,12 +1,22 @@
 import re
 
-text = "Szukamy cyfr 859 moze tutaj sie jakies ukryly?"
+text = "Szukamy cyfr 859 moze tutaj sie jakies 092 ukryly? 666"
 
 # Trenujemy regexera dajac mu instrukcje - czyli nasze wyrazenie regularne.
 regexer = re.compile(r"\d\d\d")
 
-# Teraz mowimy regexerowi, zeby szukal tego czego chcemy w podanym tekscie. Zwroci to do zmiennej res.
+# Teraz mowimy regexerowi, zeby szukal tego czego chcemy w podanym tekscie.
+# Zwroci pierwszy wynik do zmiennej res.
 res = regexer.search(text)
 
 print(text)
 print(res.group())
+
+# Jeżeli chcemy aby regexer poszukal wszystkiego co zgadza się z naszymi instrukcjami uzywamy findall
+res = regexer.findall(text)
+print(res)
+
+# albo
+res = regexer.finditer(text)
+for match in res:
+    print(match.group())
